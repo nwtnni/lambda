ec19103cf90ea055feaca787a38c6f4639716313
Newton Ni <nwtnni@gmail.com>
2018-08-06 14:45:57 -0400

    Reorganize module structure; move conversion functions into Bruijn module (since every Bruijn representation should have a naming context

--- bruijn.ml
+++ bruijn.ml
@@ -1,4 +1,33 @@
-type t =
-| Var of int
-| Abs of string * t
-| App of t * t
+module Context = struct
+  type t = string list
+
+  module S = Set.Make (String)
+  open Lambda
+
+  let from_lambda expr =
+    let rec variables = function 
+    | Var x       -> S.singleton x
+    | Abs (x, e)  -> S.add x (variables e)
+    | App (e, e') -> S.union (variables e) (variables e')
+    in
+    S.fold List.cons (variables expr) []
+
+  let push = List.cons
+
+  let find var =
+    let rec find' n = function
+    | []                  -> failwith "Unreachable by construction of context"
+    | v :: t when v = var -> n
+    | _ :: t              -> find' (n + 1) t
+    in
+    find' 0
+end
+
+module Term = struct
+  type t =
+  | Var of int
+  | Abs of string * t
+  | App of t * t
+end
+
+type t = Term.t * Context.t
--- main.ml
+++ main.ml
@@ -1,3 +1,5 @@
 open Lambda
+module B = Bruijn
+module L = Lambda
 
 let parse s = Parser.main Lexer.token (Lexing.from_string s)
