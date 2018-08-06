ad30b494eb84e9a3b85e6a5bedc3d07e7ee2b85a
Newton Ni <nwtnni@gmail.com>
2018-08-06 16:03:22 -0400

    Implement pretty-printing for named representations

--- lambda.ml
+++ lambda.ml
@@ -10,3 +10,11 @@ let rec free = function
 | Var x       -> Set.singleton x
 | Abs (x, e)  -> Set.remove x (free e)
 | App (e, e') -> Set.union (free e) (free e')
+
+let rec show = function
+| Var x                                     -> x
+| Abs (x, e)                                -> Printf.sprintf "λ%s. %s" x (show e)
+| App (Abs (_, _) as e, (Abs (_, _) as e')) -> Printf.sprintf "(%s) (%s)" (show e) (show e')
+| App (Abs (_, _) as e, e')                 -> Printf.sprintf "(%s) %s"   (show e) (show e')
+| App (e, (Abs (_, _) as e'))               -> Printf.sprintf "%s (%s)"   (show e) (show e')
+| App (e, e')                               -> Printf.sprintf "%s %s"     (show e) (show e')
--- main.ml
+++ main.ml
@@ -1,5 +1,4 @@
+open Bruijn
 open Lambda
-module B = Bruijn
-module L = Lambda
 
 let parse s = Parser.main Lexer.token (Lexing.from_string s)
