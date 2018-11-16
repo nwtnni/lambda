d730961c3e6a36a9a74bbd57785bd59caef2937b
Newton Ni <nwtnni@gmail.com>
2018-11-16 16:19:57 -0500

    First draft of pretty-printer

--- simple/lib/print.ml
+++ simple/lib/print.ml
@@ -27,12 +27,57 @@ end = struct
     | Unit  -> Format.fprintf fmt "()"
     | Var v -> Var.format_t fmt v
     | Let (v, t, e, e') ->
-      Format.fprintf fmt "@[<2>let@ %a%a =@ %a@ in@ %a@]"
+      Format.fprintf fmt "@[<2>let@ %a%a@ =@ %a@ in@ %a@]"
         Var.format_t v    
-        (format_option (fun fmt e -> Format.fprintf fmt ":@ %a " Type.format_t e)) t
+        (format_option (fun fmt e -> Format.fprintf fmt "@ :@ %a" Type.format_t e)) t
         format_t e
         format_t e'
-    | _ -> failwith "Unimplemented"
+    | Abs (v, t, e) ->
+      Format.fprintf fmt "@[<2>λ%a@ :@ %a.@ %a@]"
+        Var.format_t v
+        Type.format_t t 
+        format_t e
+    | App (e, e') ->
+      Format.fprintf fmt "@[<2>%a@ %a@]"
+        format_t e
+        format_t e'
+    | If (b, t, f) ->
+      Format.fprintf fmt "@[<2>if@ %a@ then@ %a@ else@ %a@]"
+        format_t b
+        format_t t
+        format_t f
+    | Bin (op, l, r) ->
+      Format.fprintf fmt "@[<2>%a@ %a@ %a@]"
+        format_t l
+        Bin.format_t op
+        format_t r
+    | Uno (op, e) ->
+      Format.fprintf fmt "@[<2>%a%a@]"
+        Uno.format_t op
+        format_t e
+    | Prod (l, r) ->
+      Format.fprintf fmt "@[<2>(%a,@ %a)@]"
+        format_t l
+        format_t r
+    | Pil e ->
+      Format.fprintf fmt "%a.0"
+        format_t e
+    | Pir e ->
+      Format.fprintf fmt "%a.1"
+        format_t e
+    | Inl (t, e) ->
+      Format.fprintf fmt "@[<2>inl[%a]@ %a@]"
+        Type.format_t t
+        format_t e
+    | Inr (t, e) ->
+      Format.fprintf fmt "@[<2>inr[%a]@ %a@]"
+        Type.format_t t
+        format_t e
+    | Case (e, l, r) ->
+      Format.fprintf fmt "@[<2>case@ %a@ of@ %a@ |@ %a@]"
+        format_t e
+        format_t l
+        format_t r
 end
 
 and Bin : sig
@@ -71,10 +116,21 @@ and Type : sig
   val format_t : Format.formatter -> Simple.Type.t -> unit
 end = struct
   open Simple.Type
-  let format_t fmt e =
+  let rec format_t fmt e =
     match fst e with
     | Int -> Format.fprintf fmt "int"
     | Unit -> Format.fprintf fmt "unit"
     | Bool -> Format.fprintf fmt "bool"
-    | _ -> failwith "unimplemented"
+    | Fun (l, r) ->
+      Format.fprintf fmt "%a@ ->@ %a"
+        format_t l
+        format_t r
+    | Prod (l, r) ->
+      Format.fprintf fmt "%a@ *@ %a"
+        format_t l
+        format_t r
+    | Sum (l, r) ->
+      Format.fprintf fmt "%a@ +@ %a"
+        format_t l
+        format_t r
 end
